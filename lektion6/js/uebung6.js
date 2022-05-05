394b9a29c17d62003c97574ee399df66633bd450
lebrunlouismichel <Tatimibamy#1>
2022-05-05 14:18:01 +0200

    Fäkultätiert! Fakuläsitatiert!

--- lektion6/js/uebung6.js
+++ lektion6/js/uebung6.js
@@ -6,77 +6,96 @@
  * 1) Schreibe die debug Funktion.
  */
 function aufgabe1() {
-  console.debug("Aufgabe 1 ==========================")
+    console.debug("Aufgabe 1 ==========================")
 
-  //ToDo: füge ab hier Deinen Code ein
+    //ToDo: füge ab hier Deinen Code ein
+    console.debug("Diese funktion kenne ich bereits!")
 }
 
 /**
  * 2) Benutze deinen ersten Parameter.
  */
 function aufgabe2() {
-  console.debug("Aufgabe 2 ==========================")
+    console.debug("Aufgabe 2 ==========================")
 
-  //ToDo: füge ab hier Deinen Code ein
+    //ToDo: füge ab hier Deinen Code ein
 
-  sagHallo("Peter")
+    function sagHallo(name) {
+        console.debug("Hallo i bims 1 " + name)
+    }
+sagHallo("Peter")
+
 }
 
 /**
  * 3) Benutze dein erstes Argument.
  */
 function aufgabe3() {
-  console.debug("Aufgabe 3 ==========================");
+    console.debug("Aufgabe 3 ==========================");
 
-  function sagBye(name) {
-    console.debug("Bye " + name)
-  }
-
-  //ToDo: füge ab hier Deinen Code ein
+    function sagBye(name) {
+        console.debug("Haddi tschüss " + name)
+    }
+sagBye("Peter")
+    //ToDo: füge ab hier Deinen Code ein
 }
 
 /**
  * 4) Schreibe deine erste Funktion mit Parameter und Argument.
  */
 function aufgabe4() {
-  console.debug("Aufgabe 4 ==========================");
+    console.debug("Aufgabe 4 ==========================");
 
-  //ToDo: füge ab hier Deinen Code ein
+    //ToDo: füge ab hier Deinen Code ein
+    function willkommenZurueck(name){
+        console.debug("Hallo i bims wieder, der 1 " + name)
+    }
+    willkommenZurueck("Peter")
 }
 
 /**
  * 5) Schreibe deinen ersten return.
  */
 function aufgabe5() {
-  console.debug("Aufgabe 5 ==========================");
+    console.debug("Aufgabe 5 ==========================");
 
-  function multiplizieren(a, b) {
-    //ToDo: füge ab hier Deinen Code ein
-  }
+    function multiplizieren(a, b) {
+        //ToDo: füge ab hier Deinen Code ein
+        return a * b
 
-  console.debug(multiplizieren(3, 5))
+    }
+
+    console.debug(multiplizieren(3, 5))
 }
 
 /**
  * 6) Schreibe deine erste rekursive Funktion.
  */
 function aufgabe6() {
-  console.debug("Aufgabe 6 ==========================");
+    console.debug("Aufgabe 6 ==========================");
 
-  //ToDo: füge ab hier Deinen Code ein
+    //ToDo: füge ab hier Deinen Code ein
+    function fakultaet(a){
+        if (a == 0) {
+            return 1
+        }
+        return fakultaet(a - 1) * a
 
-  console.debug(fakultaet(5))
+    }
+
+    console.debug(fakultaet(5))
 }
-
+//
 /**
  * Hauptfunktion, in der die Aufgaben aufgerufen werden
  */
 function main() {
-  aufgabe1()
-  aufgabe2()
-  aufgabe3()
-  aufgabe4()
-  aufgabe5()
-  aufgabe6()
+    aufgabe1()
+    aufgabe2()
+    aufgabe3()
+    aufgabe4()
+    aufgabe5()
+    aufgabe6()
 }
+
 main()
