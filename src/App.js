93af2b21aece8c3fe5fdea52e71902abefaf3511
lebrunlouismichel <Tatimibamy#1>
2022-05-24 15:20:14 +0200

    React 3b

--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ class App extends React.Component {
 
   erledigtAufZuKlappen() {
     // ToDo: fertig programmieren
+    let neuerZustand = !this.state.erledigtAufgeklappt
+    this.setState({erledigtAufgeklappt: neuerZustand})
   }
 
   // ToDo: diese Methode als 'checkHandler' an GruppenTag und ArtikelTag durchreichen
@@ -57,81 +59,82 @@ class App extends React.Component {
     if (this.state.einkaufenAufgeklappt == true) {
       for (const gruppe of Modell.gruppenListe) {
         nochZuKaufen.push(<GruppenTag
-          key={gruppe.id}
-          gruppe={gruppe}
-          gekauft={false}
-          aktiveGruppeHandler={() => this.setAktiveGruppe(gruppe)}/>)
+            key={gruppe.id}
+            gruppe={gruppe}
+            gekauft={false}
+            aktiveGruppeHandler={() => this.setAktiveGruppe(gruppe)}/>)
       }
     }
 
 
     let schonGekauft = []
     // ToDo: Bedingung  mit 'erledigtAufgeklappt' programmieren
-    for (const gruppe of Modell.gruppenListe) {
-      schonGekauft.push(<GruppenTag
-        key={gruppe.id}
-        gruppe={gruppe}
-        gekauft={true}/>)
+    if (this.state.erledigtAufgeklappt == true) {
+      for (const gruppe of Modell.gruppenListe) {
+        schonGekauft.push(<GruppenTag
+            key={gruppe.id}
+            gruppe={gruppe}
+            gekauft={true}/>)
+      }
     }
-
     return (
-      <div id="container">
-        {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
-        <header>
-          <h1>Watchlist</h1>
-          <label
-            className="mdc-text-field mdc-text-field--filled mdc-text-field--with-trailing-icon mdc-text-field--no-label">
-            <span className="mdc-text-field__ripple"></span>
-            <input className="mdc-text-field__input" type="search"
-                   id="artikelEingabe" placeholder="Artikel hinzufügen"/>
-            <i className="material-icons mdc-text-field__icon mdc-text-field__icon--trailing"
-               role="button">add_circle</i>
-            <span className="mdc-line-ripple"></span>
-          </label>
-        </header>
-        <hr/>
-
-        <main>
-          <section>
-            <h2>Noch zu kaufen
-              <i onClick={() => this.einkaufenAufZuKlappen()} className="material-icons">
-                {this.state.einkaufenAufgeklappt ? 'expand_more' : 'expand_less'}
-              </i>
-            </h2>
-            <dl>
-              {nochZuKaufen}
-            </dl>
-          </section>
+        <div id="container">
+          {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
+          <header>
+            <h1>Watchlist</h1>
+            <label
+                className="mdc-text-field mdc-text-field--filled mdc-text-field--with-trailing-icon mdc-text-field--no-label">
+              <span className="mdc-text-field__ripple"></span>
+              <input className="mdc-text-field__input" type="search"
+                     id="artikelEingabe" placeholder="Artikel hinzufügen"/>
+              <i className="material-icons mdc-text-field__icon mdc-text-field__icon--trailing"
+                 role="button">add_circle</i>
+              <span className="mdc-line-ripple"></span>
+            </label>
+          </header>
           <hr/>
-          <section>
-            <h2>Schon gekauft
-              {/* ToDo: füge hier drunter Deinen Code ein */}
-              <i className="material-icons">
-                expand_less
-              </i>
-            </h2>
-            <dl>
-              {schonGekauft}
-            </dl>
-          </section>
-        </main>
-        <hr/>
 
-        <footer>
-          <button className="mdc-button mdc-button--raised">
-            <span className="material-icons">bookmark_add</span>
-            <span className="mdc-button__ripple"></span> Gruppen
-          </button>
-          <button className="mdc-button mdc-button--raised">
-            <span className="material-icons">sort</span>
-            <span className="mdc-button__ripple"></span> Sort
-          </button>
-          <button className="mdc-button mdc-button--raised">
-            <span className="material-icons">settings</span>
-            <span className="mdc-button__ripple"></span> Setup
-          </button>
-        </footer>
-      </div>
+          <main>
+            <section>
+              <h2>Noch zu kaufen
+                <i onClick={() => this.einkaufenAufZuKlappen()} className="material-icons">
+                  {this.state.einkaufenAufgeklappt ? 'expand_more' : 'expand_less'}
+                </i>
+              </h2>
+              <dl>
+                {nochZuKaufen}
+              </dl>
+            </section>
+            <hr/>
+            <section>
+              <h2>Schon gekauft
+                {/* ToDo: füge hier drunter Deinen Code ein */}
+                <i onClick={() => this.erledigtAufZuKlappen()} className="material-icons">
+                  {this.state.erledigtAufgeklappt ? 'expand_more' : 'expand_less'}
+                </i>
+              </h2>
+              <dl>
+                {schonGekauft}
+              </dl>
+            </section>
+          </main>
+          <hr/>
+
+          <footer>
+            <button className="mdc-button mdc-button--raised">
+              <span className="material-icons">bookmark_add</span>
+              <span className="mdc-button__ripple"></span> Gruppen
+            </button>
+            <button className="mdc-button mdc-button--raised">
+              <span className="material-icons">sort</span>
+              <span className="mdc-button__ripple"></span> Sort
+            </button>
+            <button className="mdc-button mdc-button--raised">
+              <span className="material-icons">settings</span>
+              <span className="mdc-button__ripple"></span> Setup
+            </button>
+          </footer>
+        </div>
     )
   }
 }
