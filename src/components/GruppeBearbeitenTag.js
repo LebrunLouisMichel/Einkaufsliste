92cd39ae842d01c2305ccdbaa669311b600a8ede
lebrunlouismichel <Tatimibamy#1>
2022-05-31 08:35:41 +0200

    React 4

--- src/components/GruppeBearbeitenTag.js
+++ src/components/GruppeBearbeitenTag.js
@@ -1,39 +1,48 @@
 import React from 'react'
 
 class GruppeBearbeitenTag extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isEditing: false,
-      newName: this.props.gruppe.name
+    constructor(props) {
+        super(props)
+        this.state = {
+            isEditing: false,
+            newName: this.props.gruppe.name
+        }
     }
-  }
 
-  handleChange(event) {
-    // ToDo: implementieren
-  }
+    handleChange(event) {
+        // ToDo: implementieren
+        let gruppenName
+        this.setState({gruppenName: event.target.value})
+    }
 
-  gruppeUmbenennen(gruppe, event) {
-    if (event && event.key != "Enter") return
-    // ToDo: implementieren
-  }
+    /**
+     *
+     * @param {Gruppe} gruppe
+     * @param {Event.KEYPRESS} event
+     */
 
-  render() {
-    const gruppe = this.props.gruppe
+    gruppeUmbenennen(gruppe, event) {
+        if (event && event.key != "Enter") return
+        // ToDo: implementieren
+        gruppe.name = this.state.newName
+        this.setState({isEditing: false})
+    }
 
-    const viewTemplate = (
-      ""
-    )
-    const editTemplate = (
-      ""
-    )
+    render() {
+        const gruppe = this.props.gruppe
+        const viewTemplate = (
+            ""
+        )
+        const editTemplate = (
+            ""
+        )
 
-    return (
-      this.state.isEditing
-        ? editTemplate
-        : viewTemplate
-    )
-  }
+        return (
+            this.state.isEditing
+                ? editTemplate
+                : viewTemplate
+        )
+    }
 }
 
 export default GruppeBearbeitenTag
