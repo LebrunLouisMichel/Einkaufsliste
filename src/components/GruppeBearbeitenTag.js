ec39168051a288d4a3b2bb980fe09253e17e80cb
lebrunlouismichel <Tatimibamy#1>
2022-05-31 14:48:02 +0200

    React 4

--- src/components/GruppeBearbeitenTag.js
+++ src/components/GruppeBearbeitenTag.js
@@ -4,14 +4,15 @@ class GruppeBearbeitenTag extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            isEditing: true,
+            isEditing: false,
             newName: this.props.gruppe.name
         }
     }
 
     handleChange(event) {
         // ToDo: implementieren
-        this.setState({gruppenName: event.target.value})
+        let gruppenName = event.target.value
+        this.setState({newName: gruppenName} )
 
     }
 
@@ -21,42 +22,44 @@ class GruppeBearbeitenTag extends React.Component {
      * @param {Event.KEYPRESS} event -
      */
 
-  gruppeUmbenennen(gruppe, event) {
-    if (event && event.key != "Enter") return
-    // ToDo: implementieren
-  }
+    gruppeUmbenennen(gruppe, event) {
+        if (event && event.key != "Enter") return
+        // ToDo: implementieren
+        gruppe.name = this.state.newName
+        this.setState({isEditing: false})
+    }
 
-  render() {
-    const gruppe = this.props.gruppe
+    render() {
+        const gruppe = this.props.gruppe
 
-    const viewTemplate = (
-      <dt>
-        <span>{gruppe.name}</span>
-        <i className="material-icons"
-           onClick={() => this.setState({isEditing: true})}>
-          drive_file_rename_outline</i>
-        <i className="material-icons"
-           onClick={this.props.entfernenHandler}>delete</i>
-      </dt>
-    )
-    const editTemplate = (
-      <dt>
-        <input type="search" value={this.state.newName} autoFocus={true}
-               onChange={event => this.handleChange(event)}
-               onKeyPress={event => this.gruppeUmbenennen(gruppe, event)}/>
-        <i className="material-icons"
-           onClick={() => this.setState({isEditing: false})}>cancel </i>
-        <i className="material-icons"
-           onClick={() => this.gruppeUmbenennen(gruppe)}>check_circle </i>
-      </dt>
-    )
+        const viewTemplate = (
+            <dt>
+                <span>{gruppe.name}</span>
+                <i className="material-icons"
+                   onClick={() => this.setState({isEditing: true})}>
+                    drive_file_rename_outline</i>
+                <i className="material-icons"
+                   onClick={this.props.entfernenHandler}>delete</i>
+            </dt>
+        )
+        const editTemplate = (
+            <dt>
+                <input type="search" value={this.state.newName} autoFocus={true}
+                       onChange={event => this.handleChange(event)}
+                       onKeyPress={event => this.gruppeUmbenennen(gruppe, event)}/>
+                <i className="material-icons"
+                   onClick={() => this.setState({isEditing: false})}>cancel </i>
+                <i className="material-icons"
+                   onClick={() => this.gruppeUmbenennen(gruppe)}>check_circle </i>
+            </dt>
+        )
 
-    return (
-      this.state.isEditing
-        ? editTemplate
-        : viewTemplate
-    )
-  }
+        return (
+            this.state.isEditing
+                ? editTemplate
+                : viewTemplate
+        )
+    }
 }
 
 export default GruppeBearbeitenTag
